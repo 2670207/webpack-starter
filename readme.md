206defff3debab82f4b6d9377cf78a03f691e34a
artem aleinik <a.aleinik@beltechsoft.com>
2020-04-06 19:03:54 +0300

    build: create simple webpack starter

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Webpack Starter
+
+
+### Установка
+
+```
+# установка зависимостей
+$ npm install
+
+# сборка "production"
+$ npm run build
+
+# сборка "development"
+$ npm run dev
+
+```
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const glob = require('glob');
+
+module.exports = () => {
+  const config = {
+    entry: './src/index.js',
+    devtool: 'source-map',
+    output: {
+      filename: './scripts/app.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.pug$/,
+          loaders: ['html-loader', 'pug-html-loader?pretty'],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({ filename: './styles/style.css' }),
+    ],
+  };
+
+  // pug pages;
+  const pugPages = glob.sync(path.join(__dirname, '/src/pug/pages/*.pug'));
+  pugPages.forEach((_path) => {
+    const filename = path.basename(_path, '.pug');
+
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        filename: `${filename}.html`,
+        template: `./src/pug/pages/${filename}.pug`,
+      }),
+    );
+  });
+
+  return config;
+};
